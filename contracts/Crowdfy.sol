bbf5ca6c0afda35c08dc599606b4b8db5fb821d9
fahraday <ghosterinc@gmail.com>
2021-05-25 20:27:56 -0400

    coding principal logic

---
+++ contracts/Crowdfy.sol
@@ -0,0 +1,60 @@
+pragma solidity ^8.0.0;
+
+contract Crowdfy{
+
+    enum State {
+        Ongoing,
+        Failed, 
+        Succeded,
+        PaidOut,
+        Cancelled
+    }
+
+    struct Campaign {
+        string campaignName;
+        address beneficiary;
+        uint targetAmount;
+        uint totalCollected;
+        uint deadline;
+        bool collected;
+        State state;
+    }
+    //see options to storage the campagins.
+
+    Campaign[] public campaigns;
+
+    mapping(address => uint) hasContributed;
+    mapping (address => bool) hasCampaign;
+
+    event CampaignStarted(string campaignName, address addr, uint targetAmount, uint deadline);
+    event CampaignFinished(address addr, uint totalCollected, bool suceeded);
+
+    ///@dev: create a new campaign, only one account per active campaing. Once started they cannot update this parameters
+    function createCampaign(string memory _campaignName, uint _targetAmount, uint _deadline) public {
+        require(!hasCampaign, "Only one campaign per account");
+        campaigns.push(Campaign(_campaignName, msg.sender, _targetAmount, 0, _deadline, false, State.Ongoing));
+        emit CampaignStarted(_campaignName, msg.sender, _targetAmount, _deadline);
+        hasCampaign[msg.sender] = true;
+    }
+
+    function contribute()public payable {
+        require(!collected, "The campaign was finished");
+        totalCollected += msg.value;
+        hasContributed[msg.sender] = msg.value;
+
+        if(totalCollected >= targetAmount){
+            _closeCampaign();
+        }
+    }
+
+    //SEE IF THIS CAN BE IN SEPARATE FUNCTIONS.
+    //close the campaign once collected, failure or canceled
+    function _closeCampaign() private {
+    }
+
+    // to give all the fonds collected to the beneficiary
+    function _withdraw() internal {
+
+    }
+
+}
