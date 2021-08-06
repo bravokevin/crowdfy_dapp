f64cd9e257d6a4fd1c621c84641e7a2b65fd1791
fahraday <ghosterinc@gmail.com>
2021-08-06 14:15:07 -0400

    starting to organize the test folder structure

---
+++ test/crowdfy/contributions.js
@@ -0,0 +1,120 @@
+export default async function suite() {
+
+    it("should contribute founds", async () => {
+
+        let campaignStruct = await contract.theCampaign.call()
+        
+        let destructuredCampaign = destructCampaign(campaignStruct);
+        
+        expect(destructuredCampaign.amountRised).to.equal(0)
+        
+        await contract.contribute(
+        {
+            from: contributor1,
+            value: ONE_ETH - 200000000
+        });
+
+    campaignStruct = await contract.theCampaign.call()
+    destructuredCampaign = destructCampaign(campaignStruct)
+    expect(destructuredCampaign.amountRised).to.equal(ONE_ETH - 200000000)
+    const contributions = await contract.contributions.call(0);
+    let contributionDestructured = destructContribution(contributions);
+    expect(contributionDestructured.sender).to.equal(contributor1);
+    expect(contributionDestructured.value).to.equal(ONE_ETH - 200000000);
+    expect(contributionDestructured.numberOfContributions).to.equal(1);
+
+
+})
+
+it('should not allowed to contribute 0 < ', async () => {
+    try {
+        await contract.contribute(
+            {
+                from: contributor1,
+                value: 0
+            });
+        expect.fail()
+    }
+    catch (error) {
+        expect(error.reason).to.equal("Put a correct amount");
+    }
+})
+
+it("should not contribute during succes state", async () => {
+
+    await contract.contribute(
+        {
+            from: contributor1,
+            value: ONE_ETH
+        });
+
+    let campaignStruct = await contract.theCampaign.call()
+
+    let campaignDestructured = destructCampaign(campaignStruct);
+    expect(campaignDestructured.state).to.equal(STATE.succed)
+
+    try {
+        await contract.contribute(
+            {
+                from: contributor1,
+                value: ONE_ETH - 200000000
+            });
+
+        expect.fail()
+    } catch (err) {
+
+    }
+})
+
+it("should not contribute after deadline", async () => {
+    await contract.setDate({ from: userCampaignCreator });
+
+    try {
+        await contract.contribute(
+            {
+                from: contributor1,
+                value: ONE_ETH - 200000000
+            });
+        expect.fail()
+    }
+    catch (error) {
+        expect(error.reason).to.equal("Not Permited during this state of the campaign");
+    }
+})
+it('should have multiple contrubitions', async () =>{
+    await contract.contribute(
+        {
+            from: contributor1,
+            value: ONE_ETH / 4
+        });
+
+    await contract.contribute(
+        {
+        from: contributor1,
+        value: ONE_ETH / 4
+        });
+
+    await contract.contribute(
+        {
+        from: contributor1,
+        value: ONE_ETH / 4
+        });
+        
+    await contract.contribute(
+        {
+        from: contributor2,
+        value: ONE_ETH / 4
+        });
+
+
+    const contributions = await contract.contributionsByPeople.call(contributor1);
+    let contributionDestructured = destructContribution(contributions);
+
+    expect(contributionDestructured.sender).to.equal(contributor1);
+    expect(contributionDestructured.value).to.equal(750000000000000000);
+    expect(contributionDestructured.numberOfContributions).to.equal(3);
+
+    //NOTICE we have to know the length of the array
+    // expect(await contract.contributions.length).to.equal(4);
+})
+}
--- test/crowdfy_test.js
+++ test/crowdfy_test.js
@@ -1,6 +1,8 @@
 const CrowdfyContract = artifacts.require('./Crowdfy.sol');
 const CrowdfyFabricContract = artifacts.require('./CrowdfyFabric');
 
+const Contributions = require('crowdfy/contributions.js')
+
 
 contract('Crowdfy', (accounts) => {
     let contractFactory;
@@ -93,129 +95,7 @@ contract('Crowdfy', (accounts) => {
 
     });
 
-    describe.only('contributions', async () => {
-
-        it("should contribute founds", async () => {
-
-            let campaignStruct = await contract.theCampaign.call()
-
-            let destructuredCampaign = destructCampaign(campaignStruct);
-
-            expect(destructuredCampaign.amountRised).to.equal(0)
-
-            await contract.contribute(
-                {
-                    from: contributor1,
-                    value: ONE_ETH - 200000000
-                });
-
-            campaignStruct = await contract.theCampaign.call()
-            destructuredCampaign = destructCampaign(campaignStruct)
-            expect(destructuredCampaign.amountRised).to.equal(ONE_ETH - 200000000)
-            const contributions = await contract.contributions.call(0);
-            let contributionDestructured = destructContribution(contributions);
-            expect(contributionDestructured.sender).to.equal(contributor1);
-            expect(contributionDestructured.value).to.equal(ONE_ETH - 200000000);
-            expect(contributionDestructured.numberOfContributions).to.equal(1);
-
-
-        })
-
-        it('should not allowed to contribute 0 < ', async () => {
-            try {
-                await contract.contribute(
-                    {
-                        from: contributor1,
-                        value: 0
-                    });
-                expect.fail()
-            }
-            catch (error) {
-                expect(error.reason).to.equal("Put a correct amount");
-            }
-        })
-
-        it("should not contribute during succes state", async () => {
-
-            await contract.contribute(
-                {
-                    from: contributor1,
-                    value: ONE_ETH
-                });
-
-            let campaignStruct = await contract.theCampaign.call()
-
-            let campaignDestructured = destructCampaign(campaignStruct);
-            expect(campaignDestructured.state).to.equal(STATE.succed)
-
-            try {
-                await contract.contribute(
-                    {
-                        from: contributor1,
-                        value: ONE_ETH - 200000000
-                    });
-
-                expect.fail()
-            } catch (err) {
-
-            }
-        })
-
-        it("should not contribute after deadline", async () => {
-            await contract.setDate({ from: userCampaignCreator });
-
-            try {
-                await contract.contribute(
-                    {
-                        from: contributor1,
-                        value: ONE_ETH - 200000000
-                    });
-                expect.fail()
-            }
-            catch (error) {
-                expect(error.reason).to.equal("Not Permited during this state of the campaign");
-            }
-        })
-        it('should have multiple contrubitions', async () =>{
-            await contract.contribute(
-                {
-                    from: contributor1,
-                    value: ONE_ETH / 4
-                });
-
-            await contract.contribute(
-                {
-                from: contributor1,
-                value: ONE_ETH / 4
-                });
-
-            await contract.contribute(
-                {
-                from: contributor1,
-                value: ONE_ETH / 4
-                });
-                
-            await contract.contribute(
-                {
-                from: contributor2,
-                value: ONE_ETH / 4
-                });
-
-
-            const contributions = await contract.contributionsByPeople.call(contributor1);
-            let contributionDestructured = destructContribution(contributions);
-
-            expect(contributionDestructured.sender).to.equal(contributor1);
-            expect(contributionDestructured.value).to.equal(750000000000000000);
-            expect(contributionDestructured.numberOfContributions).to.equal(3);
-
-            //NOTICE we have to know the length of the array
-            // expect(await contract.contributions.length).to.equal(4);
-
-
-
-        })
-    })
+    describe.only('contributions', Contributions.bind(this));
 
     describe('State', async () => {
         it('should pass to state = succeded', async () => {
