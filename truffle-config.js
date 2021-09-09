4ecc875e5a3138029f4818f181d326bb47c14fd8
fahraday <ghosterinc@gmail.com>
2021-09-09 10:03:38 -0400

    contract already deployed

--- truffle-config.js
+++ truffle-config.js
@@ -59,13 +59,14 @@ module.exports = {
     ropsten: {
       provider: () => new HDWalletProvider({
         mnemonic,
-        providerOrUrl: `wss://ropsten.infura.io/ws/v3/${process.env.INFURA_API_KEY}`,
+        providerOrUrl: `https://ropsten.infura.io/v3/${process.env.INFURA_API_KEY}`,
         addressIndex: 1
       }),
+      networkCheckTimeout: 10000, 
       network_id: 3,       // Ropsten's id
       gas: 5500000,        // Ropsten has a lower block limit than mainnet
       confirmations: 2,    // # of confs to wait between deployments. (default: 0)
-      timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
+      // timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
       skipDryRun: true,     // Skip dry run before migrations? (default: false for public nets )
       numberOfAddresses: 1,
     },
